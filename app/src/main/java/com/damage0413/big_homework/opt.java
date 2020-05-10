d79067e20b09fb8cc780aa8120269fbaa907547c
日照火 <hsy00413@163.com>
2020-05-10 14:10:04 +0800

    Initial commit

---
+++ app/src/main/java/com/damage0413/big_homework/opt.java
@@ -0,0 +1,14 @@
+package com.damage0413.big_homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class opt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opt);
+    }
+}
